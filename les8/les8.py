8769379abc470580388a33819e613d68ace43fe5
S2116\r_korobkov <korobkov@salon2116.ru>
2022-11-22 18:58:09 +0300

    решаем задачу 2

--- les8/les8.py
+++ les8/les8.py
@@ -27,19 +27,26 @@ def BestResult(scores_table):
         count += 1
     return f'Победила {scores_table[int(x)][0]} группа с {len(scores_table[x]) - 1} студентами и общим баллом: {round(max, 3)}'
 
-groups = ['Первая', 'Вторая', 'Третья', 'Четвертая']
-groups_table = CreateGroups(groups)
-print(BestResult(groups_table))
-print()
+# groups = ['Первая', 'Вторая', 'Третья', 'Четвертая']
+# groups_table = CreateGroups(groups)
+# print(BestResult(groups_table))
+# print()
 
-input("Нажмите что-нибудь для выполнения след. задачи: ")
-os.system("cls")
+# input("Нажмите что-нибудь для выполнения след. задачи: ")
+# os.system("cls")
 
 # Задача 2. Дана квадратная матрица, заполненная случайными числами.
 # Определите, сумма элементов каких строк превосходит сумму главной диагонали матрицы.
 
+def CreateMatrix(count_rows: int):
+    matrix = []
+    for i in range(count_rows):
+        matrix.append(list(random.randint(0,10) for _ in range(count_rows)))
+        print(matrix[i])
+    return matrix
 
 
+CreateMatrix(int(input("Введите число: ")))
 # Задача 3. В двумерном массиве хранятся средние дневные температуры с мая по сентябрь за прошлый год.
 # Каждому месяцу соответствует своя строка. Определите самый жаркий и самый холодный 7-дневный промежуток этого периода. Выведите его даты.
 
