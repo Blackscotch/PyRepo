7ebbbde356862146a38dc72119d8ab7e94d1a509
Scotch <blackscotch@mail.ru>
2022-12-06 05:06:04 +0300

    локальные изменения

--- les10/bot_method.py
+++ les10/bot_method.py
@@ -1,30 +1,8 @@
 import datetime
 from pathlib import Path
-import xml.etree.ElementTree as xml
+import csv
 
-def writeFile(mess, mess_id, id, name, l_name, date):
-    root = xml.Element("userRequest")
-    appt = xml.Element("Quest")
-    root.append(appt)
-
-    mes_id = xml.SubElement(appt, "mes_id")
-    mes_id.text = mess_id
-
-    message = xml.SubElement(appt, "message")
-    message.text = mess
-
-    uid = xml.SubElement(appt, "uid")
-    uid.text = id
-
-    fio = xml.SubElement(appt, "fio")
-    fio.text = f"{name} {l_name}"
-
-    date_data = xml.SubElement(appt, "date_data")
-    date_data.text = f"{date.year}-{date.month}-{date.day}, {date.hour}:{date.minute}"
-
-    request = xml.SubElement(appt, "request")
-
-    tree = xml.ElementTree(root)
-    path = Path("les10", "quest.xml")
-    with open(path, 'w', encoding='utf-8') as file:
-        tree.write(file)
+def WriteFile(mess_id, mess, date, id, name, l_name, answer):
+    path = Path("les10", "quest.csv")
+    with open(path, 'a', encoding='utf-8') as file:
+        file.write('{};{};{};{};{};{};{}\n'.format(mess_id, mess, date, id, name, l_name, answer))
--- les10/les10.py
+++ les10/les10.py
@@ -1,9 +1,9 @@
 import telebot
 from telebot import types
 import datetime
-# from bot_method import *
-from pathlib import Path
-import xml.etree.ElementTree as xml
+from bot_method import *
+# from pathlib import Path
+# import xml.etree.ElementTree as xml
 
 # Задача 1. Напишите бота для техподдержки. Бот должен записывать обращения пользователей в файл.
 # Задача 2. Добавьте боту модуль, который позволяет считывать из файла вопрос,
@@ -11,32 +11,6 @@ import xml.etree.ElementTree as xml
 
 # 5951329094:AAG8xLgaX_6Zc6QyJGQh3lKuVeNLZsvN3mM
 
-def writeFile(mess):
-    root = xml.Element("userRequest")
-    appt = xml.Element("Quest")
-    root.append(appt)
-
-    mes_id = xml.SubElement(appt, "mes_id")
-    mes_id.text = "Здесь что-то"
-
-    message = xml.SubElement(appt, "message")
-    message.text = f"{mess}"
-
-    uid = xml.SubElement(appt, "uid")
-    uid.text = "Здесь айди"
-
-    fio = xml.SubElement(appt, "fio")
-    fio.text = "здесь имя и фамилия"
-
-    date_data = xml.SubElement(appt, "date_data")
-    date_data.text = "даты"
-
-    request = xml.SubElement(appt, "request")
-
-    tree = xml.ElementTree(root)
-    path = Path("les10", "quest.xml")
-    with open(path, 'w', encoding='utf-8') as file:
-        tree.write(file)
 
 bot = telebot.TeleBot("5951329094:AAG8xLgaX_6Zc6QyJGQh3lKuVeNLZsvN3mM", parse_mode=None)
 
@@ -65,9 +39,11 @@ def write_text(message):
         name = message.from_user.first_name
         l_name = message.from_user.last_name
         date = datetime.datetime.now()
+        answer = '_'
         bot.reply_to(message, f"Ваше сообщение было записано и отправлено в тех. поддержку.\n"
             f"Дата обращения: {date.hour}:{date.minute} {date.day}-{date.month}-{date.year}"
         )
-        writeFile(str(mess))
+        WriteFile(mess_id, mess, id, name, l_name, date, answer)
+
 
 bot.infinity_polling()
